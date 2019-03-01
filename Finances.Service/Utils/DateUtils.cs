d7319281bde0d6b74b526a90e0cb7d4bcfb6326d
Marianne Albuquerque da Silva <marikell_silva@hotmail.com>
2019-03-01 13:00:46 -0300

    building excel style

---
+++ Finances.Service/Utils/DateUtils.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Finances.Service.Utils
+{
+    public static class DateUtils
+    {
+        private static Dictionary<int, string> _months;
+        private static Dictionary<int, string> _formattedMonths;
+
+
+        public static Dictionary<int, string> Months
+        {
+            get
+            {
+                if(_months == null)
+                {
+                    _months = new Dictionary<int, string>();
+
+                    string[] names = DateTimeFormatInfo.CurrentInfo.MonthNames;
+
+
+                    for (int i = 0; i < 12; i++)
+                    {
+                        _months.Add(i + 1, names[i]);
+                    }
+
+
+
+                }
+
+                return _months;
+            }
+        }
+
+        public static Dictionary<int, string> FormattedMonths
+        {
+            get
+            {
+               if(_formattedMonths == null)
+                {
+                    _formattedMonths = new Dictionary<int, string>();
+
+                    foreach (var m in Months)
+                    {
+                        _formattedMonths.Add(m.Key, string.Format("{0}/{1}", m.Value, DateTime.Now.Year));
+                    }
+                }
+
+                return _formattedMonths;
+            }
+        }
+    }
+}
--- Finances.Web/Controllers/ExcelController.cs
+++ Finances.Web/Controllers/ExcelController.cs
@@ -9,6 +9,7 @@ using Syncfusion.XlsIO;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -59,7 +60,7 @@ namespace Finances.Web.Controllers
             }).ToList();
         }
 
-        private void StyleWorksheet(ref ExcelPackage package, Tuple<int,int> values)
+        private void StyleWorksheet(ref ExcelPackage package, Tuple<int, int> values)
         {
             ExcelWorksheet excelWorksheet = package.Workbook.Worksheets.FirstOrDefault();
 
@@ -80,14 +81,19 @@ namespace Finances.Web.Controllers
 
                 //Styling rows
 
-                for (int i =1; i <= values.Item1; i++)
+                for (int i = 1; i < values.Item1; i++)
                 {
                     for (int j = 0; j < values.Item2; j++)
                     {
-                        excelWorksheet.Cells[i+1, j+1].Style.Border.BorderAround(ExcelBorderStyle.Medium);
-                        excelWorksheet.Cells[i + 1, j + 1].Style.Font.Size = 10;
-                        excelWorksheet.Cells[i + 1, j + 1].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
-                        excelWorksheet.Cells[i + 1, j + 1].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                        string value = Convert.ToString(excelWorksheet.Cells[i + 1, j + 1].Value);
+
+                        if (!DateUtils.FormattedMonths.ContainsValue(value))
+                        {
+                            excelWorksheet.Cells[i + 1, j + 1].Style.Border.BorderAround(ExcelBorderStyle.Medium);
+                            excelWorksheet.Cells[i + 1, j + 1].Style.Font.Size = 10;
+                            excelWorksheet.Cells[i + 1, j + 1].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+                            excelWorksheet.Cells[i + 1, j + 1].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                        }
                     }
                 }
 
@@ -95,27 +101,46 @@ namespace Finances.Web.Controllers
 
                 for (int i = 0; i < values.Item2; i++)
                 {
-                    excelWorksheet.Cells[values.Item1 + 2, i + 1].Style.Border.BorderAround(ExcelBorderStyle.Medium);
-                    excelWorksheet.Cells[values.Item1 + 2, i + 1].Style.Font.Bold = true;
-                    excelWorksheet.Cells[values.Item1 + 2, i + 1].Style.Font.Size = 10;
-                    excelWorksheet.Cells[values.Item1 + 2, i + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
-                    excelWorksheet.Cells[values.Item1 + 2, i + 1].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml("#ff6699"));
-                    excelWorksheet.Cells[values.Item1 + 2, i + 1].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
-                    excelWorksheet.Cells[values.Item1 + 2, i + 1].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                    excelWorksheet.Cells[values.Item1, i + 1].Style.Border.BorderAround(ExcelBorderStyle.Medium);
+                    excelWorksheet.Cells[values.Item1, i + 1].Style.Font.Bold = true;
+                    excelWorksheet.Cells[values.Item1, i + 1].Style.Font.Size = 10;
+                    excelWorksheet.Cells[values.Item1, i + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                    excelWorksheet.Cells[values.Item1, i + 1].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml("#ff6699"));
+                    excelWorksheet.Cells[values.Item1, i + 1].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+                    excelWorksheet.Cells[values.Item1, i + 1].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
                 }
 
                 //merging columns of footer
-                excelWorksheet.Cells[values.Item1 + 2, 3, values.Item1 + 2, 9].Merge = true;
-                excelWorksheet.Cells[values.Item1 + 2, 3].Value = "Arquivo gerado em: " + DateTime.Now.ToString("dd-MM-yyyy HH:mm");
+                excelWorksheet.Cells[values.Item1, 3, values.Item1, 9].Merge = true;
+                excelWorksheet.Cells[values.Item1, 3].Value = "Arquivo gerado em: " + DateTime.Now.ToString("dd-MM-yyyy HH:mm");
                 excelWorksheet.Cells["A:AZ"].AutoFitColumns();
             }
 
         }
 
-        private Tuple<int,int> PrepareData(ref ExcelPackage package, ICollection<TransactionViewModel> transactionViewModels)
+        private void StyleDefaultColor(int row, int columns, ref ExcelWorksheet excelWorksheet, string hexadecimalColor)
+        {
+            for (int i = 0; i < columns; i++)
+            {
+                excelWorksheet.Cells[row, i + 1].Style.Border.BorderAround(ExcelBorderStyle.Medium);
+                excelWorksheet.Cells[row, i + 1].Style.Font.Bold = true;
+                excelWorksheet.Cells[row, i + 1].Style.Font.Size = 10;
+                excelWorksheet.Cells[row, i + 1].Style.Fill.PatternType = ExcelFillStyle.Solid;
+                excelWorksheet.Cells[row, i + 1].Style.Fill.BackgroundColor.SetColor(ColorTranslator.FromHtml(hexadecimalColor));
+                excelWorksheet.Cells[row, i + 1].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+                excelWorksheet.Cells[row, i + 1].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+            }
+
+            excelWorksheet.Cells[row, 3, row, 9].Merge = true;
+
+        }
+
+        private Tuple<int, int> PrepareData(ref ExcelPackage package, ICollection<TransactionViewModel> transactionViewModels)
         {
 
             var columns = Extension.GetPropertiesDescription<TransactionViewModel>();
+
+            int rows = 0;
 
             ExcelWorksheet excelWorksheet = package.Workbook.Worksheets.Add("Entradas/Saídas");
             for (int i = 1; i <= columns.Count(); i++)
@@ -123,25 +148,55 @@ namespace Finances.Web.Controllers
                 excelWorksheet.Cells[1, i].Value = columns.ElementAt(i - 1);
             }
 
-            for (int i = 0; i < transactionViewModels.Count; i++)
+            //for the header
+            rows += 2;
+
+            int lastMonth = 0, currentMonth = 0;
+            decimal totalByMonth = 0;
+
+            foreach (var tr in transactionViewModels)
             {
+                string dtTransaction = tr.DatTransaction;
+
+                currentMonth = Convert.ToInt32(dtTransaction.Substring(dtTransaction.IndexOf('/') + 1, 2));
+
+                if (currentMonth != lastMonth)
+                {
+                    //total by month
+                    excelWorksheet.Cells[rows, 1].Value = "R$" + Convert.ToString(transactionViewModels.Sum(o => o.VlTransaction));
+                    excelWorksheet.Cells[rows, 2].Value = totalByMonth;
+                    StyleDefaultColor(rows, columns.Count(), ref excelWorksheet, "#ffff99");
+                    totalByMonth = 0;
+                    rows++;
+
+                    StyleDefaultColor(rows, columns.Count(), ref excelWorksheet, "#b3cccc");
+                    excelWorksheet.Cells[rows, 1].Value = DateUtils.FormattedMonths[currentMonth];
+                    rows++;                    
+                }
+
+
                 for (int k = 0; k < columns.Length; k++)
                 {
-                    var transaction = transactionViewModels.ElementAt(i);
-
                     string propertyName = (Extension.GetPropertyNameByDescription<TransactionViewModel>(
                         Convert.ToString(excelWorksheet.Cells[1, k + 1].Value)));
 
-                    object value = Extension.GetPropertyValue(propertyName, transaction);
+                    object value = Extension.GetPropertyValue(propertyName, tr);
 
-                    excelWorksheet.Cells[2 + i, k + 1].Value = (value == null) ? string.Empty : Convert.ToString(value);
+                    excelWorksheet.Cells[rows, k + 1].Value = (value == null) ? string.Empty : Convert.ToString(value);
                 }
+
+                lastMonth = currentMonth;
+                totalByMonth += tr.VlTransaction;
+                rows++;
+
+                
+
             }
 
-            excelWorksheet.Cells[transactionViewModels.Count + 2, 1].Value = "Valor";
-            excelWorksheet.Cells[transactionViewModels.Count + 2, 2].Value = "R$" + Convert.ToString(transactionViewModels.Sum(o => o.VlTransaction));
+            excelWorksheet.Cells[rows, 1].Value = "Total";
+            excelWorksheet.Cells[rows, 2].Value = "R$" + Convert.ToString(transactionViewModels.Sum(o => o.VlTransaction));
 
-            return new Tuple<int, int>(transactionViewModels.Count, columns.Count());
+            return new Tuple<int, int>(rows, columns.Count());
 
         }
 
@@ -152,7 +207,7 @@ namespace Finances.Web.Controllers
         {
             ExcelPackage package = new ExcelPackage();
 
-            Tuple<int,int> values = PrepareData(ref package, GetData(dtFilter));
+            Tuple<int, int> values = PrepareData(ref package, GetData(dtFilter));
             StyleWorksheet(ref package, values);
 
             var excelBytesArray = package.GetAsByteArray();
